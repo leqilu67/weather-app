74f735e1b01fdf0a9b37b37496b8d723125c9c4e
Leqi Lu <pototoland@gmail.com>
2020-08-04 18:33:44 -0700

    base layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function WeatherCard({day, high, low, weather}) {
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+  const curDay = 0;
+  let displayDay = days[curDay+{day}];
+  return (
+    <div className = "card">
+      <div className = "card-content">
+        <span> {days[curDay+day]} </span>
+        <br />
+        <img src="https://img.icons8.com/dusk/64/000000/sun.png"/>
+        <br />
+        <span> {high} </span>
+        <span>/</span>
+        <span> {low} </span>
+      </div>
+    </div>
+  )
+}
+
+function WeatherApp() {
+
+  return (
+    <div className="weather-app">
+      <WeatherCard day={0} high={77} low={68} weather={"sunny"} />
+      <WeatherCard day={1} high={77} low={68} weather={"sunny"} />
+      <WeatherCard day={2} high={77} low={68} weather={"sunny"} />
+      <WeatherCard day={3} high={77} low={68} weather={"sunny"} />
+      <WeatherCard day={4} high={77} low={68} weather={"sunny"} />
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <WeatherApp />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
